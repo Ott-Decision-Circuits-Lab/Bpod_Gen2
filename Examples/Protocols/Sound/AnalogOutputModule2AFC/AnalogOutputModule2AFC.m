f9fee231afe9a982af5574d80c432d066f6f0059
Greg Knoll <gregory@bccn-berlin.de>
2022-09-27 18:11:53 +0200

    ADD: 2AFC analog output mod.

--- Examples/Protocols/Sound/AnalogOutputModule2AFC/AnalogOutputModule2AFC.m
+++ Examples/Protocols/Sound/AnalogOutputModule2AFC/AnalogOutputModule2AFC.m
@@ -12,6 +12,8 @@ See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Edited by gregory@bccn-berlin.de
 %}
 
 function AnalogOutputModule2AFC()
