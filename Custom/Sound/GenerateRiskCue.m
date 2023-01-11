b0ab6f6e115402237047897622a4746485b14745
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2023-01-11 17:46:53 +0100

    Shared functions for protocols

---
+++ Custom/Sound/GenerateRiskCue.m
@@ -0,0 +1,23 @@
+function Sound = GenerateRiskCue(SamplingRate, StimulusTime, mode, varargin)
+% This function uses a start and end frequencies to generate a waveform of
+% sweep, with the duration based on SamplingRate and StimulusTime
+% 
+% Sound = a vector of waveform [-1,1]
+% SamplingRate (in Hz) = Analog (usually 50kHz) or HiFi (usually 192kHz) Module's Sampling Rate
+% StimulusTime (in s) = the length of the tone duration
+% mode = a string of the style of cue generated
+% Created by antonio@bccn-berlin.de 2023-01-11
+
+switch mode
+    case 'Freq'
+        % varargin{1} in mode 'Freq' = StartFreq (in kHz) = The freqency of sine wave in the beginning of the waveform
+        % varargin{2} in mode 'Freq' = EndFreq (in kHz) = The freqency of sine wave in the end of the waveform
+        
+        StartFreq = varargin{1};
+        EndFreq = varargin{2};
+        t = 0:(1/SamplingRate):StimulusTime;
+        Sound = chirp(t, StartFreq, StimulusTime, EndFreq, 'logarithmic');
+        
+end
+
+end
---
+++ Custom/Sound/SetupHiFi.m
@@ -0,0 +1,46 @@
+function [Player,fs]=SetupHiFi(fs)
+%{
+Setup for BpodHiFi to produce sounds.
+
+See also: https://sites.google.com/site/bpoddocumentation/user-guide/function-reference/bpodwaveplayer
+
+Based on greg@bccn-berlin.de 's SetupWavePlayer(fs)
+
+Author: antonio@bccn-berlin.de 2023-01-10
+
+%}
+
+global BpodSystem
+
+if nargin < 1
+    fs = 192000;
+end
+
+%{
+---------------------------------------------------------------------------
+                                SETUP
+---------------------------------------------------------------------------
+You will need:
+- A Bpod state machine v0.7+
+- A Bpod HiFi module, loaded with Bpod BpodWavePlayer firmware.
+
+- Connect the HiFi module's State Machine port to Bpod.
+- Connect channel 1 (or ch1+2) of the analog output module to speaker(s).
+- Plug in the HiFi module to the computer via USB and start Bpod
+  in Matlab.  From the Bpod console, pfsair the serial port (left) module 
+  with its USB serial port (right).
+%}
+
+%--------------------------------------------------------------------------
+% Instantiate waveplayer and set parameters
+%--------------------------------------------------------------------------
+% Check that the HiFi Module hardware has been assigned a USB port
+BpodSystem.assertModule('HiFi', 1); % The second argument (1) indicates that the HiFi module must be paired with its USB serial port
+
+% Instantiate BpodWavePlayer object
+Player = BpodHiFi(HiFiUSB);
+Player.Port  % Prints the port to the Command Window
+
+Player.DigitalAttenuation_dB = -10;
+Player.SamplingRate = fs; %fs = 192000 % Use max (192kHz) supported sampling rate (fs = sampling freq)
+end % SetupWavePlayer
