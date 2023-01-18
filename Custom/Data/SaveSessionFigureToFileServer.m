f8a850e469acefa39ddc91107974666c633e871a
OttLabIT <116281282+OttLabIT@users.noreply.github.com>
2023-01-18 18:06:04 +0100

    Update SaveSessionFigureToFileServer.m

--- Custom/Data/SaveSessionFigureToFileServer.m
+++ Custom/Data/SaveSessionFigureToFileServer.m
@@ -2,7 +2,7 @@ function SaveSessionFigureToFileServer()
 global BpodSystem
 %save figure
 
-FigureFolder = 'O:\data'; %fullfile(fileparts(fileparts(BpodSystem.DataPath)),'Session Figures');
+FigureFolder = 'O:\share\session_figure'; %fullfile(fileparts(fileparts(BpodSystem.DataPath)),'Session Figures');
 FigureHandle = BpodSystem.GUIHandles.OutcomePlot.HandleOutcome.Parent; %FigureString = get(FigureHandle,'Name');
 [~, FigureName] = fileparts(BpodSystem.Path.CurrentDataFile);
 
