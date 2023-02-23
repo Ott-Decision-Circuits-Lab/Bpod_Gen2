9c43a70db25c7b72119b38a592865c0fd7ffafaa
Greg Knoll <gregory@bccn-berlin.de>
2023-02-23 13:00:38 +0100

    FIX: cumulative reward calculation.

--- Custom/Utils/CalculateCumulativeReward.m
+++ Custom/Utils/CalculateCumulativeReward.m
@@ -2,25 +2,27 @@ function RewardTotal = CalculateCumulativeReward()
 global BpodSystem
 
 TrialData = BpodSystem.Data.Custom.TrialData;
+RewardTotal = 0;
 
-try
-    RewardMag = TrialData.RewardMagnitude;
-    RewardTrials = TrialData.Rewarded;
+SideRewardTrials = TrialData.Rewarded;
+LeftChoices = TrialData.ChoiceLeft;
+
+if sum(SideRewardTrials)
+    LeftRewardedChoices = LeftChoices & SideRewardTrials;
+    TotalLeftReward = dot(TrialData.RewardMagnitudeL, LeftRewardedChoices);
+
+    RightRewardedChoices = ~LeftChoices & SideRewardTrials;
+    TotalRightReward = dot(TrialData.RewardMagnitudeR, RightRewardedChoices);
     
-    RewardChoices = zeros(size(RewardMag)); 
-    RewardChoices(1, TrialData.ChoiceLeft==1 & RewardTrials==1) = 1; 
-    RewardChoices(2, TrialData.ChoiceLeft==0 & RewardTrials==1) = 1;
-    side_reward_mat = RewardMag.*RewardChoices;
-    
-    try
-        center_rewards = TrialData.CenterPortRewarded;
-        center_mag = TrialData.CenterPortRewAmount(end);
-        center_reward_mat = sum(center_rewards).* center_mag;
-        RewardTotal = round(sum(side_reward_mat(:)) + sum(center_reward_mat(:)));
-    catch % if no centrer port reward is found
-        RewardTotal = round(sum(side_reward_mat(:)));
-    end
-catch  % if no reward is found, report 0uL
-    RewardTotal = 0;
+    RewardTotal = TotalLeftReward + TotalRightReward;
 end
+
+try  % not all protocols have center reward
+    CenterRewardTrials = TrialData.CenterPortRewarded;
+    CenterMag = TrialData.CenterPortRewAmount;
+
+    RewardTotal = RewardTotal + dot(CenterMag, CenterRewardTrials);
 end
+
+
+end %function
