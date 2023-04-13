1fbd5a583883f2b84102139a4f4a54c3440dd606
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2023-04-13 17:40:09 +0200

    Adjust logics and paths for photometry plot saving
    Allow 1 or 2 graphs being saved

--- Custom/Photometry/SavePhotometryFiguresToFileServer.m
+++ Custom/Photometry/SavePhotometryFiguresToFileServer.m
@@ -1,49 +1,99 @@
 function SavePhotometryFiguresToFileServer()
 global BpodSystem
 global TaskParameters
-%save figure
 
-FigureFolder = 'O:\share\session_figure'; %fullfile(fileparts(fileparts(BpodSystem.DataPath)),'Session Figures');
-
-if ~isdir(FigureFolder)
-    disp('FigureFolder is not a directory. A folder is created.')
-    mkdir(FigureFolder);
-end
-
-try
-    [~, FigureName] = fileparts(BpodSystem.Path.CurrentDataFile);
-catch
-    warning('Error: No DataFile Found. Session figure not saved to server!\n');
+%% check if there is photometry measurement
+if ~TaskParameters.GUI.Photometry
+    disp('-> No photometry measurement. No photometry figure is  saved in the file server.')
     return
 end
 
-% try
-    if TaskParameters.GUI.Photometry
-        FigureHandleNidaq1 = BpodSystem.GUIHandles.Nidaq1.fig;
-        Figure1Path = fullfile(FigureFolder,[FigureName,'_Nidaq1.png']);
-%         try
-            saveas(FigureHandleNidaq1,Figure1Path,'png');
-%         catch
-%             warning('Error: Nidaq1 figure not saved to server!\n');
-%             return
-%         end
-        disp('-> FigNidaq1 is successfully saved in file server.')
+%% check if photometry plot is created
+FigureHandleNidaq1 = [];
+try
+    FigureHandleNidaq1 = BpodSystem.GUIHandles.Nidaq1.fig;
+    disp('-> Nidaq1 photometry figure handle found. Trying to look for nidaq2 as well.')
+catch
+end
 
-        if TaskParameters.GUI.RedChannel
-            FigureHandleNidaq2 = BpodSystem.GUIHandles.Nidaq2.fig;
-            Figure2Path = fullfile(FigureFolder,[FigureName,'_Nidaq2.png']);
-%             try
-                saveas(FigureHandleNidaq2,Figure2Path,'png');
-%             catch
-%                 warning('Error: Nidaq2 figure not saved to server!\n');
-%                 return
-%             end
-            disp('-> FigNidaq2 is successfully saved in file server.')
-        end
+FigureHandleNidaq2 = [];
+try
+    FigureHandleNidaq2 = BpodSystem.GUIHandles.Nidaq2.fig; % could be only red channel is on
+    disp('-> Nidaq2 photometry figure handle found.')
+catch
+end 
+
+FigureHandles = [FigureHandleNidaq1, FigureHandleNidaq2];
+if isempty(FigureHandles)
+    disp('-> No photometry figure is save to the server.')
+    return
+end
+
+%% check if a corresponding session data and info is created
+try
+    Info = BpodSystem.Data.Info;
+catch
+    warning('No data info found. Photometry figure not saved to server!');
+    return
+end
+
+try
+    [~, FigureName, ~] = fileparts(BpodSystem.Path.CurrentDataFile);
+catch
+    warning('No data file found. Analysis figure not saved to server!');
+    return
+end
+
+%% saving 1-2 photometry figure(s) with correct indexing
+for i = 1:length(FigureHandles)
+    idx = 1;
+    if isempty(FigureHandleNidaq1)
+        idx = 2;
     end
-% catch
-%     warning('Error: photometry figurehandles not found.  Photometry figures not saved to server!\n')
-%     return
-% end
+    
+    %% check or else create folders for saving in bpod_graph (duplicated copy for cross-session view)
+    try
+        FigureFolder = strcat('\\ottlabfs.bccn-berlin.pri\ottlab\data\', Info.Subject, '\bpod_graph\');
+    catch
+        warning(strcat('Not enough data info for path definition. Nidaq figure ', num2str(i),' not saved to server!'));
+        break
+    end
 
+    if ~isfolder(FigureFolder)
+        disp('bpod_graph is not a directory. A folder is created.')
+        mkdir(FigureFolder);
+    end
+    
+    FigurePathAnalysis = fullfile(FigureFolder, [FigureName, strcat('_Nidaq', num2str(idx), '.png')]);
+    try
+        saveas(FigureHandles(i), FigurePathAnalysis, 'png');
+        disp(strcat('-> Naidaq',num2str(idx),' figure is  successfully saved in the bpod_graph folder in the file server.'))
+    catch
+        warning('Photometry figure not saved to bpod_graph folder!');
+    end
+    
+    %% check or else create folders for saving in bpod_session
+    TimestampStr = FigureName(end-14:end);
+    try
+        SessionFolder = strcat('\\ottlabfs.bccn-berlin.pri\ottlab\data\', Info.Subject, '\bpod_session\',...
+                               TimestampStr);
+    catch
+        warning('Not enough data info for path definition. Analysis figure not saved to server!');
+        return
+    end
+
+    if ~isfolder(SessionFolder)
+        disp('bpod_session is not a directory. A folder is created.')
+        mkdir(SessionFolder);
+    end
+    
+    FigurePathAnalysis = fullfile(SessionFolder, [FigureName, strcat('_Nidaq', num2str(idx), '.png')]);
+    try
+        saveas(FigureHandles(i), FigurePathAnalysis, 'png');
+        disp(strcat('-> Naidaq',num2str(idx),' figure is  successfully saved in the bpod_graph folder in the file server.'))
+    catch
+        warning('Photometry figure not saved to bpod_session folder!');
+    end
+    
+    close(FigureHandles(i));
 end
