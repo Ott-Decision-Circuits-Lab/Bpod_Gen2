ac53a9ac37b0f8d15d738628df29543682c8da31
OttLabIT <116281282+OttLabIT@users.noreply.github.com>
2023-10-17 11:19:12 +0200

    Update OttLabDataServerFolderPath.m

--- Custom/Data/OttLabDataServerFolderPath.m
+++ Custom/Data/OttLabDataServerFolderPath.m
@@ -3,6 +3,6 @@ function FolderPath = OttLabDataServerFolderPath()
 This function is for easier adjustment in case of path correction in future
 infrastructural changes
 %}
-FolderPath = '\\ottlabfs.home.bccn-berlin.de\ottlab\data\';
+FolderPath = '\\ottlabfs.bccn-berlin.pri\ottlab\data\';
 end
 
