73c1455d29127f69c7690a4333e2c7b0ef208c6e
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2023-10-24 09:56:24 +0200

    Correct pre-check logic

--- Custom/Data/RegenerateAnalysisFigure.m
+++ Custom/Data/RegenerateAnalysisFigure.m
@@ -1,11 +1,11 @@
 DataFolderPath = OttLabDataServerFolderPath();
-RatID = '1';
+RatID = '29';
 ProtocolName = 'TwoArmBanditVariant';
 sessions_path = [DataFolderPath RatID '\bpod_session'];
 session_folders = ls(sessions_path);
-for session_idx = 1: height(session_folders)
+for session_idx = 1:height(session_folders)
     session_date = session_folders(session_idx, :);
-    if any(session_date(1:4) ~= '2023')
+    if any(session_date(1:8) ~= '20230929')
         continue
     end
     
@@ -16,8 +16,9 @@ for session_idx = 1: height(session_folders)
     
     file_path = '';
     for file_idx = 1:height(files_in_data_folder)
-        if all(files_in_data_folder(file_idx, 1:string_length) == target_data_name)
+        if strcmpi(files_in_data_folder(file_idx, 1:string_length), target_data_name)
             file_path = fullfile(data_folder, files_in_data_folder(file_idx, 1:string_length));
+            continue
         end
     end
     
@@ -26,10 +27,14 @@ for session_idx = 1: height(session_folders)
     end
     
     FigHandle = Analysis(file_path);
+    if isempty(FigHandle)
+        continue
+    end
+
     FigurePathAnalysis = fullfile(data_folder, [target_data_name(1:end-4), '_Analysis.png']);
     saveas(FigHandle, FigurePathAnalysis, 'png');
 
-    sessions_path = [DataFolderPath RatID '\bpod_graph'];
-    saveas(FigHandle, sessions_path, 'png');
+    sessions_graph_path = [DataFolderPath RatID '\bpod_graph'];
+    saveas(FigHandle, sessions_graph_path, 'png');
 
 end
