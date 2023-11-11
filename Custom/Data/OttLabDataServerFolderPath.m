d772f20d790a262121a21d88107e447a2c904223
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2023-11-11 12:31:19 +0100

    Delete redundant file

--- Custom/Data/OttLabDataServerFolderPath.m
+++
@@ -1,8 +0,0 @@
-function FolderPath = OttLabDataServerFolderPath()
-%{
-This function is for easier adjustment in case of path correction in future
-infrastructural changes
-%}
-FolderPath = '\\ottlabfs.bccn-berlin.pri\ottlab\data\';
-end
-
